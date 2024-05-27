7616c8ea8e59ee8b5ddce53667277295c2e19a77
Aerin Freilich <aerinfreilich@gmail.com>
2024-05-27 14:56:06 -0400

    Add GET /files endpoint to get available files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -24,6 +25,31 @@ app.post("/upload", upload.single("audio"), (req: Request, res: Response) => {
   }
   console.log("File uploaded:", req.file);
   res.send("File uploaded successfully");
+});
+
+app.get("/files", (req: Request, res: Response) => {
+  // gets a list of files available for download
+  // TODO: accept filters in query parameters
+  const directoryPath = path.join(__dirname, "../uploads");
+
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.log({ err });
+      return res.status(500).send("Unable to scan files");
+    }
+
+    // TODO: apply filtering logic here using req.query
+
+    console.log({ files });
+    res.json({ files });
+  });
+});
+
+app.get("/metadata/:filename", (req: Request, res: Response) => {
+  // gets metadata for a specified file, accepts filters in query parameters
+  const query = req.query;
+  console.log({ query });
+  return res.status(500).send("Endpoint not yet implemented");
 });
 
 app.get("/download/:filename", (req: Request, res: Response) => {
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
-import app, { server } from "../src/index"; // Adjust the import path to your Express app
+const path = require("path");
+import app, { server } from "../src/index";
 
 describe("POST /upload", () => {
   afterAll((done) => {
@@ -20,5 +21,22 @@ describe("POST /upload", () => {
 
     expect(response.status).toBe(400);
     expect(response.text).toBe("No file uploaded.");
+  });
+
+  it("should return a list of uploaded files", async () => {
+    await request(app)
+      .post("/upload")
+      .attach("audio", path.join(__dirname, "testFiles", "test1.wav"));
+
+    await request(app)
+      .post("/upload")
+      .attach("audio", path.join(__dirname, "testFiles", "test2.wav"));
+
+    const response = await request(app).get("/files");
+
+    expect(response.status).toBe(200);
+
+    expect(response.body.files).toContain("test1.wav");
+    expect(response.body.files).toContain("test2.wav");
   });
 });
